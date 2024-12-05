6e2971fefe8d1e52ceb96b3c132155a778f78e78
luizlsl <luiz.lsldf@gmail.com>
2024-12-05 14:36:04 -0300

    scroll-suave

--- js/script.js
+++ js/script.js
@@ -45,3 +45,29 @@ function initAccordion() {
     }
 
 initAccordion()
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+
+function scrollToSection(event){
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute('href')
+    const section = document.querySelector(href)
+    
+    // Modelo antigo direto com a seção
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block:  'start'
+    })
+    
+    // Modelo de scroll suave novo
+    // const topo = section.offsetTop
+
+    // window.scrollTo({
+    //     top: topo,
+    //     behavior: 'smooth',
+    // })
+}
+
+linksInternos.forEach((link) =>{
+    link.addEventListener('click', scrollToSection)
+})
